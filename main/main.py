af1a282f8ed8864b601ed3884c4b38e41645ab41
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-01-16 14:41:08 -0700

    join and leave!

--- main/main.py
+++ main/main.py
@@ -1,9 +1,27 @@
 import discord
+import speech_recognition as sr
+import wave
 
 class botClient(discord.Client):   
-    async def on_ready(self):
-        print('Logged on as' + self.user)
-        
     
+    vc = None
+    
+    async def on_ready(self):
+        print('Logged on as' + self.user.name)
+    
+    async def on_message(self,message):
+        if(message.content == "!join"):
+            self.vc = await message.author.voice.channel.connect()
+            self.communicate()
+        elif(message.content == "!leave"):
+            if(self.vc):
+                await self.vc.disconnect()
+                
+    
+    def communicate(self):
+        pass
+
+bot = botClient()
+bot.run("TOKEN")    
         
     
