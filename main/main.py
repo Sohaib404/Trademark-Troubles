31a86e5a45e363387e5680ccc3ea0645a37aec9e
loganv90 <60831705+loganv90@users.noreply.github.com>
2021-01-16 17:43:42 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,27 +1,33 @@
 import discord
-import speech_recognition as sr
-import wave
+from discord.ext import commands
+import random
 
-class botClient(discord.Client):   
-    
-    vc = None
-    
-    async def on_ready(self):
-        print('Logged on as' + self.user.name)
-    
-    async def on_message(self,message):
-        if(message.content == "!join"):
-            self.vc = await message.author.voice.channel.connect()
-            self.communicate()
-        elif(message.content == "!leave"):
-            if(self.vc):
-                await self.vc.disconnect()
-                
-    
-    def communicate(self):
-        pass
+bot = commands.Bot(command_prefix = "!")
 
-bot = botClient()
-bot.run("TOKEN")    
-        
-    
+@bot.event
+async def on_ready():
+    print("Logged in")
+
+@bot.command()
+async def wisdom(ctx):
+    wisdoms = [
+        "Only the dead have seen the end of war.",
+        "It is forbidden to kill; therefore all murderers are punished unless they kill in large numbers and to the sound of trumpets.",
+        "Violence is the last refuge of the incompetent.",
+        "When you have to kill a man, it costs nothing to be polite.",
+        "Death, in its silent sure march is fast gathering those whom I have longest loved, so that when he shall knock at my door, I will more willingly follow.",
+        "The last enemy that shall be destroyed is death.",
+        "If you expect the worst, you'll never be disappointed.",
+        "To die will be an awfully big adventure.",
+        "One may as well be optimistic. The road to catastrophe will be rougher if it's paved with dread.",
+        "Behind every beautiful thing, there's some kind of pain.",
+        "At the point of death, the pain is over. Yeah, I guess it is a friend.",
+        "Each night, when I go to sleep, I die."
+    ]
+    await ctx.send(content=f"{random.choice(wisdoms)}", tts=True)
+
+@bot.command()
+async def clear(ctx, amount=1000):
+    await ctx.channel.purge(limit=amount)
+
+bot.run("ODAwMDg2Mjg1MTY3MzYyMDQ5.YANAaw.Nxln1b6nGLpkKBY5gWL-L86R_jY")
