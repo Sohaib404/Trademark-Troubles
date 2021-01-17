83e4bef2c4f0a37908131a0ba86e9749f8ccc7a4
AnnoyingCotton <andrewmjculberson@gmail.com>
2021-01-16 19:51:47 -0700

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,11 +1,52 @@
 import discord
 from discord.ext import commands
 from discord.voice_client import VoiceClient
-
 import random
 
-bot = commands.Bot(command_prefix = "!")
+class Player():
+    name = ""
+    presentables = []
+class bizBot():
+        
+    problems = []
+    
+    #gamephases none: no game started, starting: submitting ideas, planning: making ideas, presenting, voting
+    gamePhase = "none"
+    
+    submittingTime = 0
+    planningTime = 0
+    presentableTime = 0
+    maxPresentables = 0
+    
+    
+    players = {}
+    
+    admin = None
+    
+    def __init__(self):
+        pass
+    
 
+
+bot = commands.Bot(command_prefix="!")
+
+@bot.command()
+async def start(ctx,*,values="a"):
+    if bizBot.gamePhase == "none":
+        timers = values.split(" ")
+        timers = [int(x) for x in timers]
+        if len(timers) != 4:
+            await ctx.send("wrong amount of values.\n usage is !start !start <submitting time> <planning time> <presentable time> <max presentables>")
+        else:
+            bizBot.submittingTime = timers[0]
+            bizBot.planningTime = timers[1]
+            bizBot.presentableTime = timers[2]
+            bizBot.maxPresentables = timers[3]
+            bizBot.gamePhase = "starting"
+            bizBot.admin = ctx.message.author
+            print(timers)
+            
+        
 @bot.event
 async def on_ready():
     print("Bot Online")
@@ -29,9 +70,31 @@ async def wisdom(ctx):
     await ctx.send(content=f"{random.choice(wisdoms)}", tts=True)
 
 @bot.command()
-async def clear(ctx, *, problem="None"):
-    await ctx.channel.purge(limit=1)
-    print(problem)
+async def submit(ctx, *, problem="None"):
+    if bizBot.gamePhase == "starting":
+        await ctx.channel.purge(limit=1)
+        bizBot.problems.append(problem)
+    print(bizBot.problems)
     
+@bot.command()
+async def play(ctx):
+    if ctx.message.author not in bizBot.players and bizBot.gamePhase == "none":
+        bizBot.players.append((ctx.message.author,Player()))
+    print(bizBot.players)
+    
+@bot.command()
+async def leave(ctx):
+    if ctx.message.author in bizBot.players:
+        bizBot.players.remove(ctx.message.author)
+        
+@bot.command()
+async def done(ctx):
+    if bizBot.gamePhase == "starting":
+        bizBot.gamePhase = "planning"
+    elif bizBot.gamePhase == "planning":
+        bizBot.gamePhase = "presenting"
+    elif bizBot.gamePhase == "presenting":
+        bizBot.gamePhase = "voting"
+
 
 bot.run("ODAwMDg2Mjg1MTY3MzYyMDQ5.YANAaw.Nxln1b6nGLpkKBY5gWL-L86R_jY")
